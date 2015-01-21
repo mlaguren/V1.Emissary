325a90b1b44dbb778da63f3959067a89c6499395
Melvin Laguren <melvin.laguren@macys.com>
2015-01-21 10:30:40 -0800

    closes #1

--- application.rb
+++ application.rb
@@ -9,7 +9,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin1']
     @user = "Administrator"
   end
 end
