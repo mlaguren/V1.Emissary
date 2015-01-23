aa6c8b8d6b0b398fe8f5625f96356d136566b952
Melvin Laguren <melvin.laguren@macys.com>
2015-01-23 08:02:04 -0800

    Rewrite of V1Rss to be more object oriented

--- lib/v1rss.rb
+++ lib/v1rss.rb
@@ -1,10 +1,21 @@
 require 'rss'
 require 'open-uri'
 
-url = 'https://www12.v1host.com/MacysIncIPWSandbox/notification.v1?feed=ATOM&ticket=HFZlcnNpb25PbmUuV2ViLkF1dGhlbnRpY2F0b3JyIQAACG1sYWd1cmVutg4IKPwB0gj%2fPzf0dSjKKxCYPfha6qBBBdmN7MGv5XM4'
-open(url) do |rss|
-  feed = RSS::Parser.parse(rss)
-  title = feed.entry.title
-  puts title
-  puts title.to_s[/'*\(((B|D)-\d.*)\)/]
+class V1RSS
+  include RSS
+
+  def initialize
+    #   get rss feed url from config file
+    
+    $FEED = YAML::load(File.open("config/v1_rss.yaml"))
+  end
+
+  def get_story
+    open($FEED['rss']) do |rss|
+      feed = RSS::Parser.parse(rss)
+      title = feed.entry.title
+    end
+    return title.to_s[/'*\(((B|D)-\d.*)\)/]
+  end 
+
 end
