c59f5c5d7c06fd4e038f5b4b709cd9d4f245bd4c
Melvin Laguren <melvin.laguren@macys.com>
2015-01-26 22:10:14 -0800

    Closes #3 - rspec framework set up and written the first three tests

---
+++ spec/v1defect_spec.rb
@@ -0,0 +1,13 @@
+require './lib/v1defect'
+require 'yaml'
+
+describe V1Defect do
+
+  it "retrieves the details of a story" do
+    v1 = V1Defect.new
+    response =  v1.get_details "D-04942"
+    Number = response['Assets']['Asset']['Attribute'].map {|h1| h1['__content__'] if h1['name']=="Number"}.compact.first
+    expect(Number).to match("D-04942") 
+  end
+
+end
