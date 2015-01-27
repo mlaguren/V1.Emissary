ffe1f3c538dfb6bbeed86bd4f31c2ff14cfa12c5
Melvin Laguren <melvin.laguren@macys.com>
2015-01-26 21:56:22 -0800

    Adding initial unit tests

---
+++ spec/v1rss_spec.rb
@@ -0,0 +1,13 @@
+require './lib/v1rss'
+require 'yaml'
+
+describe V1RSS do
+  it "connect to an rss feed" do
+    test = V1RSS.new            
+  end
+
+  it "returns a story" do 
+    v1rss = V1RSS.new
+    expect(v1rss.get_story).to match(/B|D-\d.*/) 
+  end
+end
