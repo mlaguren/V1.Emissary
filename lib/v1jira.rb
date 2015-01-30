18c3d4aac6227363aa9f25fbadf5cae1afdc88f5
Melvin Laguren <melvin.laguren@macys.com>
2015-01-30 05:38:04 -0800

    v1Jira class with debug

--- lib/v1jira.rb
+++ lib/v1jira.rb
@@ -1,7 +1,11 @@
 require 'httparty'
+require 'json'
+
+require 'awesome_print'
 
 class V1Jira 
   include HTTParty
+  debug_output $stdout
 
   $JIRA = YAML::load(File.open("config/jiraconfig.yml"))
   basic_auth $JIRA['username'], $JIRA['password']
@@ -11,8 +15,23 @@ class V1Jira
   
   end
 
-  def create_ticket 
+  def create_ticket
+    payload = {:fields => {:project => {:key => "MCOMRE"}, :summary => "Test Summary", :description => "Test Description", :issuetype => {:name =>"Bug"}}}
+    puts payload.class
+    puts payload.to_json.class
+    response = self.class.post("/rest/api/2/issue/", 
+                 :body => payload.to_json, 
+                 :options => { :headers => {'Content-Type' => 'application/json' } }) 
+    return response
+  end
 
+  def get_ticket
+    ticket_details = self.class.get("/rest/api/2/issue/MCOMRE-48098")
+    ap ticket_details
   end
 
 end
+
+jira = V1Jira.new
+test = jira.create_ticket
+ap test
