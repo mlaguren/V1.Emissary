c2c9376b715d40da40bcba73186d5dac64cc2032
doublevanos <doublevanos@gmail.com>
2015-02-09 14:51:41 -0800

    Switching to instance variables

--- lib/v1mapping.rb
+++ lib/v1mapping.rb
@@ -3,30 +3,30 @@ require 'yaml'
 class V1Mapping
 
   def initialize(file1, file2)
-    $v1mapper = YAML::load(File.open(file1))
-    $v1sMapper = YAML::load(File.open(file2))
-    $rsmapper = $v1sMapper.invert
-    $rmapper = $v1mapper.invert
+    @v1mapper = YAML::load(File.open(file1))
+    @v1smapper = YAML::load(File.open(file2))
+    @rsmapper = @v1smapper.invert
+    @rmapper = @v1mapper.invert
   end
 
   def to_json
-    return $v1mapper.to_json
+    return @v1mapper.to_json
   end
   
   def get_Map
-    return $v1mapper
+    return @v1mapper
   end
 
   def get_sMap
-    return $v1sMapper
+    return @v1smapper
   end
   
   def j2v(field)
-    return $v1mapper[field]
+    return @v1mapper[field]
   end
   
   def v2j(field)
-    return $rmapper[field]
+    return @rmapper[field]
   end
     
 end
