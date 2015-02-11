ae06928037c169a5ef4ee8811fb74b44f4c2ac79
jwong <doublevanos@gmail.com>
2015-02-11 09:52:38 -0800

    whatever

--- spec/v1trigger_spec.rb
+++ spec/v1trigger_spec.rb
@@ -5,8 +5,14 @@ describe V1Trigger do
 
   it "retrieves the list of stories with JIRA flag set" do
     v1 = V1Trigger.new
-    response =  v1.get_list
-    p response
+    response =  v1.get_v1_list
+#    p response
+
+    ur = v1.get_Jira_list
+    p ur
+
+    vur = v1.get_v1defect_Jira_list
+    p vur
 
 #    Number = response['Assets']['Asset']['Attribute'].map {|h1| h1['__content__'] if h1['name']=="Number"}.compact.first
 #    expect(Number).to match("D-05106")
